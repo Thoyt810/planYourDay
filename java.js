23b6ee1174934ef5d23f297f20c8d9b9f0c0d26b
Thoyt810 <66744198+Thoyt810@users.noreply.github.com>
2020-07-06 22:34:53 -0600

    debugging and working on java

--- java.js
+++ java.js
@@ -4,10 +4,68 @@ $(document).ready(function(){
 
     $("#currentDay").append(currentDate);
 
-    $(".saveBtn").on("click", function(){
-        var userAnswer = $("textarea").val();
+    var userAnswer = $("textarea").val();
+
+    $(".saveBtn1").on("click", function(){
+        userAnswer = $("textarea").val();
         console.log(userAnswer);
-        localStorage.setItem("Things to do",  userAnswer)
+        localStorage.setItem("8 AM",  userAnswer)
+
+    })
+
+    $(".saveBtn2").on("click", function(){
+        userAnswer = $("textarea").val();
+        console.log(userAnswer);
+        localStorage.setItem("9 AM",  userAnswer)
+
+    })
+
+    $(".saveBtn3").on("click", function(){
+        userAnswer = $("textarea").val();
+        console.log(userAnswer);
+        localStorage.setItem("10 AM",  userAnswer)
+
+    })
+
+    $(".saveBtn4").on("click", function(){
+        userAnswer = $("textarea").val();
+        console.log(userAnswer);
+        localStorage.setItem("11 AM",  userAnswer)
+
+    })
+
+    $(".saveBtn5").on("click", function(){
+        userAnswer = $("textarea").val();
+        console.log(userAnswer);
+        localStorage.setItem("12 PM",  userAnswer)
+
+    })
+
+    $(".saveBtn6").on("click", function(){
+        userAnswer = $("textarea").val();
+        console.log(userAnswer);
+        localStorage.setItem("1 PM",  userAnswer)
+
+    })
+
+    $(".saveBtn7").on("click", function(){
+        userAnswer = $("textarea").val();
+        console.log(userAnswer);
+        localStorage.setItem("2 PM",  userAnswer)
+
+    })
+
+    $(".saveBtn8").on("click", function(){
+        userAnswer = $("textarea").val();
+        console.log(userAnswer);
+        localStorage.setItem("3 PM",  userAnswer)
+
+    })
+
+    $(".saveBtn9").on("click", function(){
+        userAnswer = $("textarea").val();
+        console.log(userAnswer);
+        localStorage.setItem("4 PM",  userAnswer)
 
     })
 
